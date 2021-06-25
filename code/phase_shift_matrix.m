5daf8f573a9d5c814c1ad9f4c026dec5f57b3ec8
Abhishek Halder <halder.abhishek@gmail.com>
2021-06-25 13:05:35 -0700

    updating Section V

--- code/phase_shift_matrix.m
+++ code/phase_shift_matrix.m
@@ -1,21 +1,13 @@
 function phi = phase_shift_matrix(Y)
 
-m = size(Y);
-nc = m(2);
+nc = size(Y,2);
 
 for i = 1:nc
-    
-   
   for j = 1:nc
-   
      if i == j
-         
          phi(i,j) = 0;
-         
      else 
          phi(i,j) = -atan2(real(Y(i,j)),imag(Y(i,j)));
      end
   end
-    
-    
 end
